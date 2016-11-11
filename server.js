1e5d7803f6a3d2958712f9875794755a165206d7
Tarragon <me@withtarragon.com>
2016-11-11 15:03:46 +0000

    implementing timestamp api exercise

--- server.js
+++ server.js
@@ -1,10 +1,50 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var strftime = require('strftime');
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/:time', function(req, res) {
+    var time = req.params.time;
+    var result;
+    var date;
+    var unixTm;
+    try {
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+        if (time.toString().match(/^[0-9]+$/)) {
+            unixTm = time;
+            date = strftime('%B %d, %Y', new Date(unixTm * 1000));
+        }
+        else {
+            unixTm = Date.parse(time);
+            if (isNaN(unixTm)) {
+                unixTm = null;
+                date = null;
+            }
+            else {
+                date = strftime('%B %d, %Y', new Date(unixTm));
+                unixTm = unixTm / 1000;
+            }
+        }
+
+        result = {
+            "unix": unixTm,
+            "natural": date
+        };
+
+    }
+    catch (ex) {
+        result = {
+            "unix": null,
+            "natural": null
+        };
+
+    }
+    res.send(result);
+});
+
+app.get('*', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(8080, function() {
+    console.log('Example app listening on port 8080!')
 })
